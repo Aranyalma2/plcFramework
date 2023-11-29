7376cf2202c8596677dcd81f75e985b1b3a87eca
Aranyalma2 <40268481+Aranyalma2@users.noreply.github.com>
2023-11-29 01:06:55 +0100

    description added

--- src/blocks/Logical/Logical_AND.cpp
+++ src/blocks/Logical/Logical_AND.cpp
@@ -1,29 +1,33 @@
 #include "Logical_AND.h"
 
+// Constructor for Logical_AND class
 Logical_AND::Logical_AND(uint16_t unique_id) : FunctionBlock(unique_id, inputs, inputConstants, INPUT_LENGTH, outputs, OUTPUT_LENGTH) {}
 
+// Perform logical AND operation
 float Logical_AND::And()
 {
   return static_cast<float>(this->castToLogical(*inputs[0]) && this->castToLogical(*inputs[1]));
 }
 
+// Invert the input value
 float Logical_AND::inverter(float in)
 {
-  // output is inverted
+  // Output inverter
   if (in == static_cast<float>(0))
     return static_cast<float>(1);
   else
     return static_cast<float>(0);
 }
 
+// Run the Logical_AND block
 uint8_t Logical_AND::run()
 {
   if (this->checkValidity())
   {
-    outputs[0] = this->And();
-    outputs[1] = this->inverter(outputs[0]);
-    return 0;
+    outputs[0] = this->And();                // Execute the logical AND operation
+    outputs[1] = this->inverter(outputs[0]); // Invert the result and store it in the second output
+    return 0;                                // Return success
   }
   else
-    return 1;
+    return 1; // Return failure if inputs are not valid
 }
--- src/blocks/Logical/Logical_AND.h
+++ src/blocks/Logical/Logical_AND.h
@@ -2,12 +2,51 @@
 #define LOGICAL_AND_H
 
 #include "../FunctionBlock.h"
-/* Logical AND
+
+/*
+ * Logical_AND - Function Block for Logical AND Operation
+ *
+ * Description:
+ * This class defines a Function Block, Logical_AND, for performing the logical AND operation.
+ * It inherits from the FunctionBlock class and takes two input values and produces two outputs:
+ *   - Output: Represents the result of the logical AND operation between input 1 and input 2.
+ *   - Inverted Output: Represents the logical NOT of the result (inverted output).
+ *
  * Inputs:
- * - input 1
- * - input 2
- * - output
- * - inverted output
+ *   - input 1
+ *   - input 2
+ *
+ * Outputs:
+ *   - output: Result of the logical AND operation (input 1 AND input 2)
+ *   - inverted output: Logical NOT of the result (inverted output)
+ *
+ * Member Variables:
+ *   - INPUT_LENGTH: Number of input values (2 in this case)
+ *   - OUTPUT_LENGTH: Number of output values (2 in this case)
+ *   - inputs[2]: Array to store pointers to input values
+ *   - inputConstants[2]: Array to store constant values (currently initialized to zero)
+ *   - outputs[2]: Array to store the results of the logical AND operation and its inversion
+ *
+ * Member Functions:
+ *   - Logical_AND(uint16_t unique_id = 0): Constructor for Logical_AND, initializes the object with a unique ID.
+ *   - virtual uint8_t run() override: Executes the logical AND operation and updates the output arrays.
+ *   - float And(): Performs the logical AND operation between input 1 and input 2.
+ *   - float inverter(float in): Calculates the logical NOT (inversion) of the input.
+ *
+ * Usage:
+ *   - Create an instance of Logical_AND, set input values using inputs[], and call run() to perform
+ *     the logical AND operation.
+ *
+ * Example:
+ *   ```
+ *   Logical_AND andBlock(456);
+ *   andBlock.inputs[0] = 1.0; // True
+ *   andBlock.inputs[1] = 0.0; // False
+ *   andBlock.run();
+ *   float result = andBlock.outputs[0]; // Result will be 0.0 (False)
+ *   float invertedResult = andBlock.outputs[1]; // Inverted result will be 1.0 (True)
+ *   ```
+ *
  */
 
 class Logical_AND : public FunctionBlock

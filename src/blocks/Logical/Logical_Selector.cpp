3e6db469ce1f579bccf56ef75a7ec8c90644d21f
Aranyalma2 <40268481+Aranyalma2@users.noreply.github.com>
2023-11-29 01:36:30 +0100

    description added

--- src/blocks/Logical/Logical_Selector.cpp
+++ src/blocks/Logical/Logical_Selector.cpp
@@ -1,27 +1,26 @@
 #include "Logical_Selector.h"
 
-Logical_Selector::Logical_Selector(uint16_t unique_id) : FunctionBlock(unique_id, inputs, inputConstants, INPUT_LENGTH, outputs, OUTPUT_LENGTH)
-{
-}
+// Constructor for Logical_Selector class
+Logical_Selector::Logical_Selector(uint16_t unique_id) : FunctionBlock(unique_id, inputs, inputConstants, INPUT_LENGTH, outputs, OUTPUT_LENGTH) {}
 
+// Perform the logical selection operation
 float Logical_Selector::select()
 {
-
-  
-  // selector "bit" if not 0 select 1, else 0
+  // Selector "bit" if not 0, select input 1, else select input 0
   return this->castToLogical(*inputs[2]) ? *inputs[1] : *inputs[0];
-
 }
 
+// Run the Logical_Selector block
 uint8_t Logical_Selector::run()
 {
+  // Check if inputs are valid
   if (this->checkValidity())
   {
-    outputs[0] = this->select();
-    return 0;
+    outputs[0] = this->select(); // Execute the logical selection operation
+    return 0;                    // Return success
   }
   else
   {
-    return 1;
+    return 1; // Return failure if inputs are not valid
   }
 }
--- src/blocks/Logical/Logical_Selector.h
+++ src/blocks/Logical/Logical_Selector.h
@@ -2,12 +2,48 @@
 #define LOGICAL_SELECTOR_H
 
 #include "../FunctionBlock.h"
-/* Logical selector block select from 2 inputs by a selector value
+
+/*
+ * Logical_Selector - Function Block for Logical Selection
+ *
+ * Description:
+ * This class defines a Function Block, Logical_Selector, for selecting between two input values based on a selector value.
+ * It inherits from the FunctionBlock class and takes three input values, producing one output value.
+ *
  * Inputs:
- * - Select input 1
- * - Select input 2
- * - Selector value
- * NOT IMPLEMENTED - Selection limit (example, if limit==0, and selector value==0 selected will be first if value==3, it will be the second one)
+ *   - Select input 1: First input value for selection
+ *   - Select input 2: Second input value for selection
+ *   - Selector value: Value to determine which input to select
+ *
+ * Outputs:
+ *   - Output: Selected output value based on the selector value
+ *
+ * Member Variables:
+ *   - INPUT_LENGTH: Number of input values (3 in this case)
+ *   - OUTPUT_LENGTH: Number of output values (1 in this case)
+ *   - inputs[3]: Array to store pointers to input values
+ *   - inputConstants[3]: Array to store constant values (currently initialized to zero)
+ *   - outputs[1]: Array to store the selected output value
+ *
+ * Member Functions:
+ *   - Logical_Selector(uint16_t unique_id = 0): Constructor for Logical_Selector, initializes the object with a unique ID.
+ *   - virtual uint8_t run() override: Executes the logical selection operation and updates the output array.
+ *   - float select(): Selects the output value based on the selector value and input values.
+ *
+ * Usage:
+ *   - Create an instance of Logical_Selector, set input values using inputs[], and call run() to perform
+ *     the logical selection operation.
+ *
+ * Example:
+ *   ```
+ *   Logical_Selector selectorBlock();
+ *   selectorBlock.setInput(0, 1.0) // First input value
+ *   selectorBlock.setInput(1, 2.0) // Second input value
+ *   selectorBlock.setInput(2, 1.0) // Selector value
+ *   selectorBlock.run();
+ *   float result = selectorBlock.getOutput(0); // Result will be 2.0 (selected the second input)
+ *   ```
+ *
  */
 
 class Logical_Selector : public FunctionBlock
@@ -19,7 +55,7 @@ private:
   float inputConstants[3] = {0};
   float outputs[1] = {0};
 
-  float select();
+  float select(); // Perform the logical selection operation
 
 public:
   Logical_Selector(uint16_t unique_id = 0);
@@ -27,4 +63,4 @@ public:
   virtual uint8_t run() override;
 };
 
-#endif
+#endif // LOGICAL_SELECTOR_H

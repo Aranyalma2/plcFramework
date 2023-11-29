9ff27e8cf22a2e134451571807d91adb3e74158e
Aranyalma2 <40268481+Aranyalma2@users.noreply.github.com>
2023-11-29 01:33:48 +0100

    description added

--- src/blocks/Logical/Logical_OR.cpp
+++ src/blocks/Logical/Logical_OR.cpp
@@ -1,31 +1,36 @@
 #include "Logical_OR.h"
 
+// Constructor for Logical_OR class
 Logical_OR::Logical_OR(uint16_t unique_id) : FunctionBlock(unique_id, inputs, inputConstants, INPUT_LENGTH, outputs, OUTPUT_LENGTH) {}
 
+// Perform the logical OR operation
 float Logical_OR::Or()
 {
+  // Output is the logical OR of the two input values
   return static_cast<float>(this->castToLogical(*inputs[0]) || this->castToLogical(*inputs[1]));
 }
 
+// Invert the input value
 float Logical_OR::inverter(float in)
 {
-  // output is inverted
   if (in == static_cast<float>(0))
     return static_cast<float>(1);
   else
     return static_cast<float>(0);
 }
 
+// Run the Logical_OR block
 uint8_t Logical_OR::run()
 {
+  // Check if inputs are valid
   if (this->checkValidity())
   {
-    outputs[0] = this->Or();
-    outputs[1] = this->inverter(outputs[0]);
-    return 0;
+    outputs[0] = this->Or();                 // Execute the logical OR operation
+    outputs[1] = this->inverter(outputs[0]); // Invert the result and store it in the second output
+    return 0;                                // Return success
   }
   else
   {
-    return 1;
+    return 1; // Return failure if inputs are not valid
   }
 }
--- src/blocks/Logical/Logical_OR.h
+++ src/blocks/Logical/Logical_OR.h
@@ -2,11 +2,49 @@
 #define LOGICAL_OR_H
 
 #include "../FunctionBlock.h"
-/* Logical OR
- * - input 1
- * - input 2
- * - output
- * - inverted output
+
+/*
+ * Logical_OR - Function Block for Logical OR Operation
+ *
+ * Description:
+ * This class defines a Function Block, Logical_OR, for performing the logical OR operation.
+ * It inherits from the FunctionBlock class and takes two input values, producing two output values.
+ *
+ * Inputs:
+ *   - input 1: First input value for the logical OR operation
+ *   - input 2: Second input value for the logical OR operation
+ *
+ * Outputs:
+ *   - output: Result of the logical OR operation
+ *   - inverted output: Inverted result of the logical OR operation
+ *
+ * Member Variables:
+ *   - INPUT_LENGTH: Number of input values (2 in this case)
+ *   - OUTPUT_LENGTH: Number of output values (2 in this case)
+ *   - inputs[2]: Array to store pointers to input values
+ *   - inputConstants[2]: Array to store constant values (currently initialized to zero)
+ *   - outputs[2]: Array to store the result of the logical OR operation
+ *
+ * Member Functions:
+ *   - Logical_OR(uint16_t unique_id = 0): Constructor for Logical_OR, initializes the object with a unique ID.
+ *   - virtual uint8_t run() override: Executes the logical OR operation and updates the output array.
+ *   - float Or(): Performs the logical OR operation on the input values.
+ *   - float inverter(float in): Inverts the input value.
+ *
+ * Usage:
+ *   - Create an instance of Logical_OR, set input values using inputs[], and call run() to perform
+ *     the logical OR operation.
+ *
+ * Example:
+ *   ```
+ *   Logical_OR orBlock(0);
+ *   orBlock.setInput(0, 1.0) // True
+ *   orBlock.setInput(1, 0.0) // False
+ *   orBlock.run();
+ *   float result = orBlock.getOutput(); // Result will be 1.0 (True)
+ *   float invertedResult = orBlock.getOutput(1); // Inverted result will be 0.0 (False)
+ *   ```
+ *
  */
 
 class Logical_OR : public FunctionBlock

2e1333a552682ddf720a35d5a2e0f1d137e2a3f3
Aranyalma2 <40268481+Aranyalma2@users.noreply.github.com>
2023-11-29 01:24:32 +0100

    description added

--- src/blocks/Logical/Logical_Counter.cpp
+++ src/blocks/Logical/Logical_Counter.cpp
@@ -9,43 +9,51 @@
  *  4 - reset value 0/1 pulse
  *  5 - reset detaction mode (low to high or high to low)
  */
+#include "Logical_Counter.h"
 
+// Constructor for Logical_Counter class
 Logical_Counter::Logical_Counter(uint16_t unique_id) : FunctionBlock(unique_id, inputs, inputConstants, INPUT_LENGTH, outputs, OUTPUT_LENGTH) {}
 
+// Perform the logical counting operation
 float Logical_Counter::counter()
 {
+  // Extract logic values from input signals
   uint8_t inputState = this->castToLogical(*inputs[0]);
-  uint8_t inputDetactionMode = this->castToLogical(*inputs[1]);
+  uint8_t inputDetectionMode = this->castToLogical(*inputs[1]);
 
   uint8_t resetState = this->castToLogical(*inputs[4]);
-  uint8_t resetDetactionMode = this->castToLogical(*inputs[5]);
+  uint8_t resetDetectionMode = this->castToLogical(*inputs[5]);
 
-  if (firstRun)
-  {
-    // defaut value
-    outputs[0] = *inputs[2];
-    // work mode change last states to inverse
-    if (inputDetactionMode == 1)
+  /*
+    // Initialization on the first run
+    if (firstRun)
     {
-      lastInput = 1;
-    }
-    if (resetDetactionMode == 1)
-    {
-      lastReset = 1;
-    }
-    firstRun = 0;
-  }
+      // Initialize with the default value
+      outputs[0] = *inputs[2];
 
-  // Check if the input value has changed since last time
+      // If the input or reset detection mode is high to low, change last states to inverse
+      if (inputDetectionMode == 1)
+      {
+        lastInput = 1;
+      }
+      if (resetDetectionMode == 1)
+      {
+        lastReset = 1;
+      }
+      firstRun = 0; // Set firstRun flag to false after initialization
+    }
+    */
+
+  // Check if the input value has changed since the last time
   if (inputState != lastInput)
   {
     // Input value has changed, check if it has gone from high to low or low to high
-    if (inputDetactionMode == 1)
+    if (inputDetectionMode == 1)
     { // High to low
       if (inputState == 0 && lastInput == 1)
       {
         lastInput = inputState;
-        return outputs[0] + *inputs[3];
+        return outputs[0] + *inputs[3]; // Increment the counter
       }
     }
     else
@@ -53,21 +61,21 @@ float Logical_Counter::counter()
       if (inputState == 1 && lastInput == 0)
       {
         lastInput = inputState;
-        return outputs[0] + *inputs[3];
+        return outputs[0] + *inputs[3]; // Increment the counter
       }
     }
-    lastInput = inputState;
+    lastInput = inputState; // Update lastInput with the current input state
   }
 
-  // reset check
+  // Reset check
   if (resetState != lastReset)
   {
-    if (resetDetactionMode == 1)
+    if (resetDetectionMode == 1)
     { // High to low
       if (lastReset == 1 && resetState == 0)
       {
         lastReset = resetState;
-        return *inputs[2];
+        return *inputs[2]; // Reset the counter
       }
     }
     else
@@ -75,24 +83,26 @@ float Logical_Counter::counter()
       if (lastReset == 0 && resetState == 1)
       {
         lastReset = resetState;
-        return *inputs[2];
+        return *inputs[2]; // Reset the counter
       }
     }
-    lastInput = inputState;
+    lastInput = inputState; // Update lastInput with the current input state
   }
 
-  return outputs[0];
+  return outputs[0]; // Return the current counter value
 }
 
+// Run the Logical_Counter block
 uint8_t Logical_Counter::run()
 {
+  // Check if inputs are valid
   if (this->checkValidity())
   {
-    outputs[0] = this->counter();
-    return 0;
+    outputs[0] = this->counter(); // Execute the logical counting operation
+    return 0;                     // Return success
   }
   else
   {
-    return 1;
+    return 1; // Return failure if inputs are not valid
   }
 }
--- src/blocks/Logical/Logical_Counter.h
+++ src/blocks/Logical/Logical_Counter.h
@@ -2,14 +2,62 @@
 #define LOGICAL_COUNTER_H
 
 #include "../FunctionBlock.h"
-/* Logical Counter input detaction high to low or low to high with start and way controlling.
+
+/*
+ * Logical_Counter - Function Block for Logical Counter count by pulse detection
+ *
+ * Description:
+ * This class defines a Function Block, Logical_Counter, for counting pulses based on input parameter.
+ * It inherits from the FunctionBlock class and extends it by counting up or down edge of a pulse,
+ * specifying the start number, controlling the counting direction and amount, reset by up or down edge of another pulse.
+ *
  * Inputs:
- * - input value 0/1 pulse
- * - input detaction mode (low to high or high to low)
- * - counter start number
- * - count way (up or down) 1/-1
- * - reset value 0/1 pulse
- * - reset detaction mode (low to high or high to low)
+ *   - input value: 0/1 pulse representing the input signal
+ *   - input detection mode: Specifies the transition (low to high or high to low) to trigger counting
+ *   - counter start number: Initial value of the counter
+ *   - count way: Specifies the counting direction (up or down) with values 1 or -1, respectively
+ *   - reset value: 0/1 pulse representing the reset signal
+ *   - reset detection mode: Specifies the transition (low to high or high to low) to trigger resetting
+ *
+ * Outputs:
+ *   - output: Counter state
+ *
+ * Member Variables:
+ *   - INPUT_LENGTH: Number of input values (6 in this case)
+ *   - OUTPUT_LENGTH: Number of output values (1 in this case)
+ *   - inputs[6]: Array to store pointers to input values
+ *   - inputConstants[6]: Array to store constant values (currently initialized to zero)
+ *   - outputs[1]: Array to store the result of the logical counting operation
+ *   - lastInput: Holds the last value of the input for detecting changes
+ *   - lastReset: Holds the last value of the reset signal for detecting changes
+ *   - firstRun: Flag indicating the first execution of the run function
+ *
+ * Member Functions:
+ *   - Logical_Counter(uint16_t unique_id = 0): Constructor for Logical_Counter, initializes the object with a unique ID.
+ *   - virtual uint8_t run() override: Executes the logical counting operation based on the input signals.
+ *   - float counter(): Performs the counting operation based on input signals and updates the counter value.
+ *
+ * Usage:
+ *   - Create an instance of Logical_Counter, set input values using inputs[], and call run() to perform
+ *     the logical counting operation.
+ *
+ * Example:
+ *   ```
+ *   Logical_Counter counterBlock();
+ *   counterBlock.setInput(0, 1.0); // Input pulse
+ *   counterBlock.setInput(1, 1.0); // High to low transition detection
+ *   counterBlock.setInput(2, 10.0); // Counter start number
+ *   counterBlock.setInput(3, 1.0); // Count up by 1
+ *   counterBlock.setInput(4, 0.0); // Reset pulse
+ *   counterBlock.setInput(5, 0.0); // Low to high transition detection for reset
+ *   counterBlock.run();
+ *
+ *   counterBlock.setInput(0, 0.0); // Change input pulse
+ *   counterBlock.run();
+ *
+ *   float result = counterBlock.getOutput(); // result = 11
+ *   ```
+ *
  */
 
 class Logical_Counter : public FunctionBlock

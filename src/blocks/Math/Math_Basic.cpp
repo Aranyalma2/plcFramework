eedaaab09c7575d1a685ac4264f0e4832780588c
Aranyalma2 <40268481+Aranyalma2@users.noreply.github.com>
2023-11-29 01:00:51 +0100

    0-9 functions support
     * Supported Operations:
     *   0: Addition
     *   1: Subtraction
     *   2: Multiplication
     *   3: Division (handles division by zero)
     *   4: Absolute value
     *   5: Exponentiation (base raised to the power exponent)
     *   6: nth root (exponentiation with 1/n as the exponent)
     *   7: Logarithm (B to the base A)
     *   8: Maximum of two values
     *   9: Minimum of two values

--- src/blocks/Math/Math_Basic.cpp
+++ src/blocks/Math/Math_Basic.cpp
@@ -1,3 +1,59 @@
+/*
+   File: Math_Basic.cpp
+   Author: Nemeth Balint
+
+   Description:
+   This file defines the implementation of the Math_Basic class, which is a subclass of FunctionBlock.
+   It provides basic mathematical operations such as addition, subtraction, multiplication, division,
+   absolute value, exponentiation, logarithms, maximum, and minimum.
+
+   Class Overview:
+   - Math_Basic: Handles various mathematical operations based on the input parameters.
+                 Inherits from FunctionBlock.
+
+   Member Functions:
+   - Math_Basic(uint16_t unique_id): Constructor initializes the Math_Basic object with a unique ID,
+                                     sets up input and output arrays.
+
+   - float math(): Performs the mathematical operation based on the specified operation code.
+                   Returns the result of the operation.
+
+   - uint8_t run(): Checks the validity of inputs and executes the mathematical operation.
+                   Updates the output array with the result.
+
+   Supported Operations (specified by *inputs[2]):
+   0: Addition
+   1: Subtraction
+   2: Multiplication
+   3: Division (handles division by zero)
+   4: Absolute value
+   5: Exponentiation (base raised to the power exponent)
+   6: nth root (exponentiation with 1/n as the exponent)
+   7: Logarithm (B to the base A)
+   8: Maximum of two values
+   9: Minimum of two values
+
+   Note: If the specified operation code is not recognized, the default return value is 0.0.
+
+   Dependencies:
+   - FunctionBlock: Base class providing the structure for function blocks.
+
+   Usage:
+   - Create an instance of Math_Basic, set input values using inputs[], and call run() to perform
+     the specified mathematical operation.
+
+   Example:
+   ```
+    Math_Basic mathBlock();
+    mathBlock.inputs[0] = 5.0;
+    mathBlock.inputs[1] = 3.0;
+    mathBlock.inputs[2] = 0; // Addition
+    mathBlock.run();
+    float result = mathBlock.outputs[0]; // Result will be 8.0
+    ```
+
+*/
+
 #include "Math_Basic.h"
 
 Math_Basic::Math_Basic(uint16_t unique_id) : FunctionBlock(unique_id, inputs, inputConstants, INPUT_LENGTH, outputs, OUTPUT_LENGTH) {}
@@ -6,44 +62,40 @@ float Math_Basic::math()
 {
   switch (static_cast<uint8_t>(*inputs[2]))
   {
-  // ADD
   case 0:
-  {
-    return *inputs[0] + *inputs[1];
-  }
-  // SUBTRACT
+    return *inputs[0] + *inputs[1]; // Addition
   case 1:
-  {
-    return *inputs[0] - *inputs[1];
-  }
-  // MULTIPLY
+    return *inputs[0] - *inputs[1]; // Subtraction
   case 2:
-  {
-    return *inputs[0] * *inputs[1];
-  }
-  // DIVIDE
+    return *inputs[0] * *inputs[1]; // Multiplication
   case 3:
-  {
-    if (*inputs[1] == 0.0)
-    {
-      return 0.0;
-    }
-    return *inputs[0] / *inputs[1];
-  }
+    return (*inputs[1] != 0.0) ? (*inputs[0] / *inputs[1]) : 0.0; // Division (handle division by zero)
+  case 4:
+    return abs(*inputs[0]); // Absolute Value
+  case 5:
+    return pow(*inputs[0], *inputs[1]); // Exponentiation
+  case 6:
+    return (*inputs[0] != 0.0) ? pow(*inputs[1], 1.0 / *inputs[0]) : 0.0; // nth Root (handle division by zero)
+  case 7:
+    return (*inputs[1] != 0.0 || *inputs[1] != 0.0) ? (log(*inputs[1]) / log(*inputs[0])) : 0.0; // Logarithm (handle any log(0))
+  case 8:
+    return max(*inputs[0], *inputs[1]); // Maximum
+  case 9:
+    return min(*inputs[0], *inputs[1]); // Minimum
+
   default:
-  {
-    return 0.0;
-  }
+    return 0.0; // Default case if operation code is not recognized
   }
 }
 
+// Run the Math_Basic block
 uint8_t Math_Basic::run()
 {
   if (this->checkValidity())
   {
-    outputs[0] = this->math();
-    return 0;
+    outputs[0] = this->math(); // Execute the mathematical operation
+    return 0;                  // Return success
   }
   else
-    return 1;
+    return 1; // Return failure if inputs are not valid
 }
--- src/blocks/Math/Math_Basic.h
+++ src/blocks/Math/Math_Basic.h
@@ -2,11 +2,57 @@
 #define __MATH_BASIC_H__
 
 #include "../FunctionBlock.h"
-/* Math-Basic
+/*
+ * Math_Basic - Function Block for Basic Mathematical Operations
+ *
+ * Description:
+ * This class defines a Function Block, Math_Basic, for performing basic mathematical operations.
+ * It inherits from the FunctionBlock class and provides functionality for addition, subtraction,
+ * multiplication, and division based on two input values and an operation code.
+ *
+ * Supported Operations:
+ *   0: Addition
+ *   1: Subtraction
+ *   2: Multiplication
+ *   3: Division (handles division by zero)
+ *   4: Absolute value
+ *   5: Exponentiation (base raised to the power exponent)
+ *   6: nth root (exponentiation with 1/n as the exponent)
+ *   7: Logarithm (B to the base A)
+ *   8: Maximum of two values
+ *   9: Minimum of two values
+ *
  * Inputs:
- * - input 1
- * - input 2
- * - operation (ADD/SUB/MUL/DIV)
+ *   - input 1
+ *   - input 2
+ *   - operation (0-9 to specify the operation)
+ *
+ * Member Variables:
+ *   - INPUT_LENGTH: Number of input values (3 in this case)
+ *   - OUTPUT_LENGTH: Number of output values (1 in this case)
+ *   - inputs[3]: Array to store pointers to input values
+ *   - inputConstants[3]: Array to store constant values (currently initialized to zero)
+ *   - outputs[1]: Array to store the result of the mathematical operation
+ *
+ * Member Functions:
+ *   - Math_Basic(uint16_t unique_id = 0): Constructor for Math_Basic, initializes the object with a unique ID.
+ *   - virtual uint8_t run() override: Executes the mathematical operation and updates the output array.
+ *   - float math(): Performs the mathematical operation based on the specified operation code.
+ *
+ * Usage:
+ *   - Create an instance of Math_Basic, set input values using inputs[], and call run() to perform
+ *     the specified mathematical operation.
+ *
+ * Example:
+ *   ```
+ *   Math_Basic mathBlock(123);
+ *   mathBlock.inputs[0] = 5.0;
+ *   mathBlock.inputs[1] = 3.0;
+ *   mathBlock.inputs[2] = 0; // Addition
+ *   mathBlock.run();
+ *   float result = mathBlock.outputs[0]; // Result will be 8.0
+ *   ```
+ *
  */
 
 class Math_Basic : public FunctionBlock

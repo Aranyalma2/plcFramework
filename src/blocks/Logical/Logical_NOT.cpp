474fc52a825d3604a6ea78d7371d9d8d0294f1f4
Aranyalma2 <40268481+Aranyalma2@users.noreply.github.com>
2023-11-29 01:29:01 +0100

    description added

--- src/blocks/Logical/Logical_NOT.cpp
+++ src/blocks/Logical/Logical_NOT.cpp
@@ -1,23 +1,26 @@
 #include "Logical_NOT.h"
 
-Logical_NOT::Logical_NOT(uint16_t unique_id) : FunctionBlock(unique_id, inputs, inputConstants, INPUT_LENGTH, outputs, OUTPUT_LENGTH)
-{
-}
+// Constructor for Logical_NOT class
+Logical_NOT::Logical_NOT(uint16_t unique_id) : FunctionBlock(unique_id, inputs, inputConstants, INPUT_LENGTH, outputs, OUTPUT_LENGTH) {}
 
+// Perform the logical NOT operation
 float Logical_NOT::negation()
 {
-  // output is inverted
+  // Output is inverted
   return static_cast<float>(!this->castToLogical(*inputs[0]));
-
 }
 
+// Run the Logical_NOT block
 uint8_t Logical_NOT::run()
 {
+  // Check if inputs are valid
   if (this->checkValidity())
   {
-    outputs[0] = this->negation();
-    return 0;
+    outputs[0] = this->negation(); // Execute the logical NOT operation
+    return 0;                      // Return success
   }
   else
-    return 1;
+  {
+    return 1; // Return failure if inputs are not valid
+  }
 }
